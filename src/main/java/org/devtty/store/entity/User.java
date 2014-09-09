02d681727aa80279f8769b0280fd15bd6b37f47c
Denis Renning <denis@devtty.de>
2014-09-09 14:59:10 +0200

    security

--- src/main/java/org/devtty/store/entity/User.java
+++ src/main/java/org/devtty/store/entity/User.java
@@ -27,8 +27,7 @@ public class User implements Serializable {
     private String login;
     private String pass;
     private String email;
-    
-    
+    private String role;
     
     private boolean accounting;
     
@@ -39,6 +38,47 @@ public class User implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
 
     @Override
     public int hashCode() {
