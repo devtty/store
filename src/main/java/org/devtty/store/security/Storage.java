4d83bf3e4dec9636ad559af2641501f0292051f3
Denis Renning <denis@devtty.de>
2015-05-22 13:35:30 +0200

    new roles

---
+++ src/main/java/org/devtty/store/security/Storage.java
@@ -0,0 +1,22 @@
+package org.devtty.store.security;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import org.apache.deltaspike.security.api.authorization.SecurityBindingType;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+/**
+ *
+ * @author Denis Renning <denis at devtty.de>
+ */
+@Retention(value = RUNTIME)
+@Target({TYPE, METHOD})
+@Documented
+@SecurityBindingType
+public @interface Storage {
+    
+}
