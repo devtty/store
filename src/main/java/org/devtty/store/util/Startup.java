6a151dbcc6152b8b616ad0a5d2219d596b7459d9
Denis Renning <denis@devtty.de>
2015-05-24 20:03:26 +0200

    add some users just for development

--- src/main/java/org/devtty/store/util/Startup.java
+++ src/main/java/org/devtty/store/util/Startup.java
@@ -8,7 +8,10 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.identity.Group;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.deltaspike.core.api.projectstage.ProjectStage;
 import org.devtty.store.entity.Config;
+import org.devtty.store.entity.User;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.Search;
 import org.slf4j.Logger;
@@ -22,6 +25,9 @@ public class Startup {
  
     @Inject 
     Logger logger;
+    
+    @Inject 
+    ProjectStage projectStage;
     
     @Inject
     EntityManager entityManager;
@@ -74,9 +80,13 @@ public class Startup {
             identityService.saveGroup(g);
         }
 
-        logger.info("B");
+        //just for dev TODO remove later (incl. method)
+        if(ProjectStage.Development.equals(this.projectStage)){
+            addSomeUsers();
+        }
         
-        createIndizies();entityManager.close();
+        createIndizies();
+        entityManager.close();
     }
     
     
@@ -89,5 +99,20 @@ public class Startup {
             logger.error("Error while indexing");
         }
         
+    }
+
+    private void addSomeUsers() {
+        logger.debug("addSomeUsers");
+        for(int i = 0; i < 300; i++){
+            User u = new User();
+            u.setLogin(RandomStringUtils.randomAlphabetic(8));
+            u.setName(RandomStringUtils.randomAlphabetic(8));
+            u.setPass(RandomStringUtils.randomAlphabetic(8));
+            u.setEmail(RandomStringUtils.randomAlphanumeric(4) + "@devtty.de");
+            u.setRole("user");
+            entityManager.persist(u);
+        }
+        
+        entityManager.flush();
     }
 }
