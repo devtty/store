6805b949a09ff93d7ad0db21c56ffa349b29d36d
Denis Renning <denis@devtty.de>
2018-09-23 17:42:53 +0200

    functional test

--- src/test/java/org/devtty/store/test/functional/ItemFunctionalTest.java
+++ src/test/java/org/devtty/store/test/functional/ItemFunctionalTest.java
@@ -1,7 +1,11 @@
 package org.devtty.store.test.functional;
 
+import org.jboss.arquillian.graphene.GrapheneElement;
 import org.jboss.arquillian.junit.Arquillian;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.support.FindBy;
 
 /**
  *
@@ -10,6 +14,18 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class ItemFunctionalTest extends FunctionalTest{
     
+    @FindBy(id = "formContent:itemTable")
+    private GrapheneElement datatable;
     
+    @Test
+    public void test(){
+        
+        browser.get(deploymentUrl.toExternalForm() + "/items/index.jsf");
+        
+        assertTrue("itemtable not present", datatable.isPresent());
+        System.out.print(browser.getTitle());
+        assertTrue("fff" + browser.getTitle(), browser.getTitle().equals("Store"));
+                
+    }
     
 }
